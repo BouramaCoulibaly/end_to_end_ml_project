fb659a596d5d1e8c27c28a40956c055a3dd5db1e
BouramaCoulibaly <coulouba2001@gmail.com>
2023-03-24 05:06:38 +0000

    update pipeline

--- application.py
+++ application.py
@@ -11,8 +11,12 @@ app=application
 
 ## Route for a home page
 
-@app.route('/',methods=['GET','POST'])
+@app.route('/')
 def index():
+    return render_template('index.html') 
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
@@ -22,8 +26,8 @@ def index():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
+            reading_score=float(request.form.get('writing_score')),
+            writing_score=float(request.form.get('reading_score'))
 
         )
         pred_df=data.get_data_as_data_frame()
@@ -32,14 +36,6 @@ def index():
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
         return render_template('home.html',results=results[0])
-    
-
-       
-
-
-
-
-
     
 
 if __name__=="__main__":
