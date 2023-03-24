5424b4b2e1c626f4c1f22a556b34c5d482ad78be
BouramaCoulibaly <coulouba2001@gmail.com>
2023-03-24 09:21:11 +0000

    update pipeline

--- application.py
+++ application.py
@@ -15,29 +15,57 @@ app=application
 #def index():
     #return render_template('home.html') 
 
+import logging
+
+# create logger
+logger = logging.getLogger('my_logger')
+logger.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create file handler and set level to debug
+fh = logging.FileHandler('app.log')
+fh.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to handlers
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+
+# add handlers to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
+
 @app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            
-            reading_score=float(request.form.get('reading_score')),
-            
-            writing_score=float(request.form.get('writing_score'))
+        try:
+            data=CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('writing_score')),
+                writing_score=float(request.form.get('reading_score'))
+            )
 
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
+            pred_df=data.get_data_as_data_frame()
+            print(pred_df)
 
-        predict_pipeline=PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
+            predict_pipeline=PredictPipeline()
+            results=predict_pipeline.predict(pred_df)
+            return render_template('home.html',results=results[0])
+        except Exception as e:
+            logger.error(str(e))
+            return "An error occurred"
+
     
 
 if __name__=="__main__":
